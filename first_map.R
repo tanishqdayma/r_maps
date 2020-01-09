18b93a143e47cb73e760cae836d17c303d950bf7
Tanishq Dayma <48204617+tanishqdayma@users.noreply.github.com>
2020-01-09 14:15:48 +0530

    Update first_map.R

--- first_map.R
+++ first_map.R
@@ -1,33 +1,30 @@
-#install.packages("sf")
-#install.packages("tmap")
-#install.packages("tmaptools")
-library(devtools)
-install_github("mtennekes/tmaptools")
-install_github("mtennekes/tmap")
-
+install.packages("sf")
 library(sf)
 library(ggplot2)
 library(tmap)
 library(tmaptools)
 library(leaflet)
+
 library(dplyr)
 
 options(scipen = 999)
 
 mydata <- readr::read_csv("median-household-income-county-2017.csv")
 
-mymap <- st_read("./shp/ne_110m_admin_0_countries.shp", stringAsFactors = FALSE)
+#mymap <- st_read("./shp/ne_110m_admin_0_countries.shp", stringAsFactors = FALSE)
+mymap <- readOGR(dsn = "./shp/", "ne_110m_admin_0_countries")
 str(mymap)
 
 
 map_and_data <- merge(mymap, mydata, by=NULL)
-
+  
 str(map_and_data)
 
-ggplot(map_and_data)
-  geom_sf(aes(fill = MedianIncome))
+ggplot(mymap)  
+  geom_sf(aes(fill = Name))
   
 ggplot(map_and_data) + 
   geom_sf(aes(fill = MedianIncome)) +
   scale_fill_gradient(low = "#5681F7", high = "#132843")
+
 
